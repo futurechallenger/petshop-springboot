b7513764cf986abd08b70e799fdf1eab02dc1296
Uncle Charlie <sharp.bruce.lee@gmail.com>
2020-08-22 21:51:21 +0800

    first controller

---
+++ src/main/kotlin/com/petshop/core/GreetingController.kt
@@ -0,0 +1,17 @@
+package com.petshop.core
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class GreetingController {
+
+  @GetMapping("/greetings")
+  fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) : ResponseEntity<Map<String, String>>{
+    try{
+      return ResponseEntity.status(200).body(mapOf("Hello  $name" to "message"))
+    } catch(e: Exception) {
+      return ResponseEntity.status(500).body(mapOf(e.localizedMessage to "message"))
+    }
+  }
+}
